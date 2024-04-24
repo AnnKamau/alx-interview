bfe9b016b7ede7b886fbcc24ca930dda05c0fa21
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-04-24 09:04:50 +0300

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -12,7 +12,9 @@ Returns an empty list if n <= 0
     
     for i in range(1, n):
         row = [1]
+        
         for j in range(1, i):
+            
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
         row.append(1)
         triangle.append(row)
