df8f5f5db93b9f218108400aba2b42b2310b4617
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-04-30 11:24:01 +0300

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,4 +2,27 @@
 """
 Write a method that determines if all the boxes can be opened.
 """
-def canUnlockAll(boxes)
+def canUnlockAll(boxes):
+    """
+    Lockboxes
+
+    Arguments:
+    n (int): number of locked boxes in front of you
+
+    Return:
+    True if all boxes can be opened, else return False
+    """
+  if not boxes:
+    return False
+  
+  keys = set([0])
+  new_keys = set([0])
+
+  while new_keys:
+    next_keys = set()
+    for key in new_keys:
+      next_keys.update(set(boxes[key]) - keys)
+    keys.update(new_keys)
+    new_keys = next_keys - keys
+
+  return len(keys) == len(boxes)
