9641413e7714d8ac040aa3a8ecaf64e96f5c6035
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-05-07 16:15:05 +0300

    Create 0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Minimum Operations
+"""
+def minOperations(n):
+    if n <= 1:
+        return n
+
+    operations = 0
+    current_h = 1
+    while current_h < n:
+        if n % current_h == 0:
+            operations += current_h  # Copy all 'H' and paste n // current_h times
+            current_h *= n // current_h
+        else:
+            operations += current_h  # Copy all 'H' and paste once
+            current_h += current_h    # Update current_h to be doubled
+
+    return operations
