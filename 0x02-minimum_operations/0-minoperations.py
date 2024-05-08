bf7c66e477eb92bed11d4ca8882b0925bc4cc41e
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-05-08 12:21:30 +0300

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -13,9 +13,11 @@ def minOperations(n):
     Returns an integer
     If n is impossible to achieve, return 0
     """
-    if n <= 1:
+   if n <= 1:
         return 0
     
     for i in range(2, n + 1):
         if n % i == 0:
             return i + minOperations(n // i)
+    
+    return 0
