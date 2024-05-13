01db023666164653deecfc479a9935d8a5be0b12
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-05-13 12:21:59 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 """
-Log parsing
+Reads stdin line by line and computes metrics:
 """
 import sys
 
 """
+Log parsing
 
+    Arguments:
+    n (int): Line by line and computer metrics
+
+    Return:
+    Number of lines by status code:
+    possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
+    if a status code doesn’t appear or is not an integer, don’t print anything for this status code
+    format: <status code>: <number>
+    status codes should be printed in ascending orde
 """
 # Initialize variables
 total_file_size = 0
