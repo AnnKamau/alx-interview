e4b38ef65b5f8fffa48ca786c79fc9da754b0b1a
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-06-19 09:05:54 +0300

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
 """
-Fewest number of coins needed to meet a given amount 
+Module to determine the fewest number of coins needed to meet a given amount total.
 """
+
 def makeChange(coins, total):
-  """
-  Prints the changes made to get total
-  
-  Arguments:
-  n (int): The value of a coin will always be an integer greater than 0
-  
-  Return:
-  If total is 0 or less, return 0
-  If total cannot be met by any number of coins you have, return -1
-  """
+    """
+    Determine the fewest number of coins needed to meet total.
+    
+    Args:
+    coins (list of int): List of the values of the coins in your possession.
+    total (int): The target amount.
+    
+    Returns:
+    int: Fewest number of coins needed to meet total, or -1 if total cannot be met.
+    """
     if total <= 0:
         return 0
     
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
-
+    
     for coin in coins:
         for i in range(coin, total + 1):
             dp[i] = min(dp[i], dp[i - coin] + 1)
