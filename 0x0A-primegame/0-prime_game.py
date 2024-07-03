b794b4011b3e4c0fd76e5f77226e0f60509a1c4b
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-07-03 09:08:28 +0300

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,57 +1,48 @@
 #!/usr/bin/python3
 """
-prime numbers based on a game.
+Prime numbers game.
 """
 
 def isWinner(x, nums):
     """
-    prime numbers based on a game.
-    
-    Args:
-    where x is the number of rounds and nums is an array of n
-    
-    Returns:
-    name of the player that won the most rounds
-    If the winner cannot be determined, return None
-    You can assume n and x will not be larger than 10000
-    """
-    def is_prime(num):
-        """Helper function to check if a number is prime"""
-        if num <= 1:
-            return False
-        if num <= 3:
-            return True
-        if num % 2 == 0 or num % 3 == 0:
-            return False
-        i = 5
-        while (i * i) <= num:
-            if num % i == 0 or num % (i + 2) == 0:
-                return False
-            i += 6
-        return True
+    Determine the winner of the game.
 
+    Args:
+    x (int): number of rounds
+    nums (list): array of n values for each round
+
+    Returns:
+    str: name of the player that won the most rounds
+    None: if the winner cannot be determined
+    """
+    
     def generate_primes_up_to(n):
         """Helper function to generate all prime numbers up to n"""
         sieve = [True] * (n + 1)
+        sieve[0] = sieve[1] = False
         p = 2
-        while (p * p) <= n:
-            if sieve[p] == True:
+        while p * p <= n:
+            if sieve[p]:
                 for i in range(p * p, n + 1, p):
                     sieve[i] = False
             p += 1
         return [p for p in range(2, n + 1) if sieve[p]]
-
+    
     def play_game(n):
         """Simulate the game and return the winner for the given n"""
+        if n < 2:
+            return "Ben"
+        
         primes = generate_primes_up_to(n)
-        turn = 0  # Maria starts
+        turn = 0
+        
         while primes:
             prime = primes[0]
             primes = [num for num in primes if num % prime != 0]
             turn = 1 - turn
 
         return "Maria" if turn == 1 else "Ben"
-
+    
     maria_wins = 0
     ben_wins = 0
 
