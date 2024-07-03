cbdc028098e5f71d8035a09db820a37100442792
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-07-03 09:01:16 +0300

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,62 +3,50 @@
 prime numbers based on a game.
 """
 
-def sieve_of_eratosthenes(max_num):
-    """
-    Prime numbers
-    Arguments:
-    where x is the number of rounds and nums is an array of n
-
-    Returns:
-    name of the player that won the most rounds
-    If the winner cannot be determined, return None
-    You can assume n and x will not be larger than 10000
-    """
-
-    is_prime = [True] * (max_num + 1)
-    p = 2
-    while (p * p <= max_num):
-        if (is_prime[p] == True):
-            for i in range(p * p, max_num + 1, p):
-                is_prime[i] = False
-        p += 1
-    is_prime[0], is_prime[1] = False, False
-    return [p for p, prime in enumerate(is_prime) if prime]
-
 def isWinner(x, nums):
-    max_n = max(nums)
-    primes = sieve_of_eratosthenes(max_n)
+    def is_prime(num):
+        """Helper function to check if a number is prime"""
+        if num <= 1:
+            return False
+        if num <= 3:
+            return True
+        if num % 2 == 0 or num % 3 == 0:
+            return False
+        i = 5
+        while (i * i) <= num:
+            if num % i == 0 or num % (i + 2) == 0:
+                return False
+            i += 6
+        return True
+
+    def generate_primes_up_to(n):
+        """Helper function to generate all prime numbers up to n"""
+        sieve = [True] * (n + 1)
+        p = 2
+        while (p * p) <= n:
+            if sieve[p] == True:
+                for i in range(p * p, n + 1, p):
+                    sieve[i] = False
+            p += 1
+        return [p for p in range(2, n + 1) if sieve[p]]
+
+    def play_game(n):
+        """Simulate the game and return the winner for the given n"""
+        primes = generate_primes_up_to(n)
+        turn = 0  # Maria starts
+        while primes:
+            prime = primes[0]
+            primes = [num for num in primes if num % prime != 0]
+            turn = 1 - turn
+
+        return "Maria" if turn == 1 else "Ben"
 
     maria_wins = 0
     ben_wins = 0
 
     for n in nums:
-        prime_set = set(primes)
-        current_set = set(range(1, n + 1))
-
-        while prime_set:
-            # Maria's turn
-            maria_choice = min(prime_set)
-            for num in range(maria_choice, n + 1, maria_choice):
-                if num in current_set:
-                    current_set.remove(num)
-                    prime_set.discard(num)
-
-            if not prime_set:
-                break
-
-            # Ben's turn
-            ben_choice = min(prime_set)
-            for num in range(ben_choice, n + 1, ben_choice):
-                if num in current_set:
-                    current_set.remove(num)
-                    prime_set.discard(num)
-
-            if not prime_set:
-                break
-
-        # Determine the winner of this round
-        if len(prime_set) % 2 == 1:
+        winner = play_game(n)
+        if winner == "Maria":
             maria_wins += 1
         else:
             ben_wins += 1
